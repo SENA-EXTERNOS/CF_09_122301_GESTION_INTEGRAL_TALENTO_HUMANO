471e29830316a8c878c88751b064e665dc57c964
jorozcop <orojacobo@gmail.com>
2023-10-11 16:38:10 -0500

    Ajuste 11-10-2023

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -15,9 +15,9 @@
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Relacionar términos
+              p.fw-bold.mb-0 Falso o verdadero
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad1/index.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/index.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece                     
 </template>
